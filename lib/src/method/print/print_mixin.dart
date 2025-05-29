a714f5d29ddcd44afff8c4774537d6b32e0c4f7e
Nguyễn Minh Hưng <90996598+nguyenminhhung2011@users.noreply.github.com>
2025-05-29 14:32:25 +0700

    🗑️ chore: add export

--- lib/src/method/print/print_mixin.dart
+++ lib/src/method/print/print_mixin.dart
@@ -5,6 +5,6 @@ import 'package:urovo_flutter/src/utils/channel_tag.dart';
 mixin PrintMixin {
   static const platform = MethodChannel(ChannelTag.channel);
   Future<void> onPrint({required PrintModel printModel}) async {
-    await platform.invokeMethod(ChannelTag.methodPrint, printModel.toJson);
+    await platform.invokeMethod(ChannelTag.methodPrint, printModel.toJson());
   }
 }
--- lib/urovo.dart
+++ lib/urovo.dart
@@ -1,4 +1,7 @@
 export 'package:urovo_flutter/src/urovo_service.dart';
 export 'package:urovo_flutter/src/urovo_widget.dart';
 export 'package:urovo_flutter/src/utils/channel_tag.dart';
-export 'package:urovo_flutter/src/method/scanner/urovo_scanner_service.dart';
+export 'package:urovo_flutter/src/method/scanner/scanner_service.dart';
+export 'package:urovo_flutter/src/method/scanner/object/scanner_object.dart';
+export 'package:urovo_flutter/src/method/beeper/beeper_object.dart';
+export 'package:urovo_flutter/src/method/print/print_object.dart';
